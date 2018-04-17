1d464f6b3df708e0f651f536628860be4c69873c
Jaykumarp1 <jaykumarp1559@gmail.com>
2018-04-17 15:41:34 +0530

    added the image through paperclip,need to config testcase for it

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -69,6 +69,6 @@ class NewsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def news_params
-      params.require(:news).permit(:s_no, :img, :title, :url)
+      params.require(:news).permit(:s_no, :img, :title, :url, :image)
     end
 end
